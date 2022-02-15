71b809bfa375d559eba13667eee2555450e056be
ac <7801186+ac137@users.noreply.github.com>
2022-02-14 20:37:26 -0500

    more ensemble uncert testing

--- calc_ice_thickness_uncert_ensemble.py
+++ calc_ice_thickness_uncert_ensemble.py
@@ -9,7 +9,7 @@ is2_data = xr.open_dataset('gridded_freeboard_2019-03.nc')
 
 OIB_STATUS = 'averaged'
 EXTRA_FMT = 'final_5k_2018_2019_cov'
-
+DATA_FLAG = 'oib_averaged_ensemble_uncert'
 
 # open all the ensembe members and concatenate into a big dataset
 nesosim_data = xr.open_mfdataset('/users/jk/19/acabaj/nesosim_uncert_output_oib_{}{}/100km/ERA*/final/*.nc'.format(OIB_STATUS,EXTRA_FMT),combine='nested',concat_dim='iteration_number')
@@ -44,25 +44,36 @@ print('monthly mean snow density')
 print(r_s)
 
 # looks like we get the right shape now!
-# # # freeboard error
-# # e_h_f = is2_data['freeboard uncertainty'].values[0,:,:]
+# # freeboard error # don't need this
+# e_h_f = is2_data['freeboard uncertainty'].values[0,:,:]
 
-# # ice density error
-# # e_r_i = 10 #kg/m^3 based on Alexandrov et al. (2013)
-# # snow depth
+# ice density error
+# e_r_i = 10 #kg/m^3 based on Alexandrov et al. (2013)
+# snow depth
 
-# h_s = nesosim_data_monthly['snow_depth'].mean(axis=1).values
-# # freeboard height
-# h_f = is2_data['freeboard'].values[0,:,:]
-
-
-
-# # 1/(r_w-r_s) because this term shows up a lot
-# inverse_r_w_minus_r_i = 1/(r_w - r_i)
+h_s = nesosim_data_monthly['snow_depth'].mean(axis=1).values
+# freeboard height from is2
+h_f = is2_data['freeboard'].values[0,:,:]
 
 
-# ens_sea_ice_thickness = h_f*r_w*inverse_r_w_minus_r_i + h_s*(r_s-r_w)*inverse_r_w_minus_r_i
 
-# # this would now be an n_iter*90*90 array?
+# 1/(r_w-r_s) because this term shows up a lot
+inverse_r_w_minus_r_i = 1/(r_w - r_i)
 
-# sea_ice_uncert = 
+
+# do the math; get a sea ice thickness ensemble (hopefully it doesn't complain about dimension mismatch? may need transpose)
+ens_sea_ice_thickness = h_f*r_w*inverse_r_w_minus_r_i + h_s*(r_s-r_w)*inverse_r_w_minus_r_i
+
+# this would now be an n_iter*90*90 array?
+
+# calculate standard deviation to produce an uncertainty estimate!
+sea_ice_uncert = np.std(ens_sea_ice_thickness,axis=0)
+
+print(sea_ice_uncert)
+print(sea_ice_uncert.shape) #should be 90x90 if all goes well...
+
+
+plt.figure()
+plt.imshow(sea_ice_uncert,origin='lower',vmin=0,vmax=5)
+plt.colorbar()
+plt.savefig('sea_ice_thickness_uncert_estimate_{}.png'.format(DATA_FLAG))
